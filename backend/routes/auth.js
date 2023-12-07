17464fd975d2515ac103a0c3e321b44ffee3dc5f
atul-mandackar <atul007atulmandavkar@gmail.com>
2023-12-07 16:48:51 +0530

    Hash password making. Here we converting plain text password to hash password and making them hard to be hacked. We are also adding salt technique to plain text password before convering to hash. First our plain text password is minimum of 8 length and after adding salt we increase its size so it become unable to hecked even if our database is leaked. For this we firstly install bcriptjs npm package by writing "npm i bcryptjs" in terminal. Then we inport the bcriptjs by code "const bcrypt = require('bcryptjs');" at top of page in auth.js Then before saving New User to the database, we add salt to plain text password and convert thet password to hash For that we handle user (schema) of User (model) and in user we used password by writing user["password], this way we cah handle the value change of schema model data. So first created salt by writing code "const salt = bcrypt.genSaltSync(10);" then make hash by code "const hash = bcrypt.hashSync(user["passwod"], salt)"; Then we saved that changed password to User model by code "user["password] = hash";
    This will make hard to hack for hackedr even if our database is leaked because in database there is hashed password in password of User, so hacker cannot decode original password from that vey easily even if he has rainbow table.

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
 const { body, validationResult } = require("express-validator");
+const bcrypt = require('bcryptjs');
 
 // Create a User using : POST "r/api/auth/createUser"  and also doesn't require auth. where createUser is in api/auth
 // use post method instead of get method of router because of user data is shown in get method but not in post method.
@@ -13,32 +14,35 @@ router.post(
     body("password", "Minimum length is 8 for password").isLength({ min: 8 }),
   ],
   async (req, res) => {
-    console.log(req.body);
+    //console.log(req.body); // to check xontent in req.body
     const result = validationResult(req);
     if (!result.isEmpty()) {
       result.array().forEach((e) => console.log(e.msg));
-      return res.status(400).send({ errors: result.array() });  
+      return res.status(400).send({ errors: result.array() });
     }
 
-    try{
-      const isUser = await User.findOne({email: req.body.email}); // to find the user with same email already exist or not
-    //console.log(isUser);
-    if(isUser){
-      console.log("Email alredy exists");
-      return res.status(400).send({error: "Email already exists"})
-    }
-    const user = new User(req.body);
-    await user
-      .save()
-      .catch((err) => console.log(err))
-      .then(console.log("user is saved on database"));
-    return res.send(req.body);
-    }
-    catch(error){
+    try {
+      const isUser = await User.findOne({ email: req.body.email }); // to find the user with same email already exist or not
+      //console.log(isUser);
+      if (isUser) {
+        console.log("Email alredy exists");
+        return res.status(400).send({ error: "Email already exists" });
+      }
+      const user = new User(req.body);
+      //console.log(user["password"]); // in model User there is schema user which have key as password
+      // befor Use saved we need to make plain text password to hash password (secure password)
+      const salt = await bcrypt.genSaltSync(10); // async method for genating salt to make password more strong, salt is added to password by backend itself, no one know what is the value of salt
+      const hash = await bcrypt.hash(user["password"], salt); // async method for convering normal plain text password to hash password along with adding salt before hashing.
+      user["password"] = hash; // changing password in model User where schema name is user.
+      await user
+        .save()
+        .catch((err) => console.log(err))
+        .then(console.log("user is saved on database"));
+      return res.send(req.body);
+    } catch (error) {
       console.error(error.message);
       res.status(500).send("some error occured");
     }
-    
   }
 );
 
