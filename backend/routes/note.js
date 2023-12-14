c3cb3d1ecad9f679b6d7ebd50f9aae48026cda62
atul-mandackar <atul007atulmandavkar@gmail.com>
2023-12-14 20:27:05 +0530

    Adding note and fetching all notes. We create two routes in note.js file, one for fetching all notes and other for saving a note in user database. For both the process we use the middleware named fetchuser which gives us the user id from token because these both routes works only when user is logged in. So for both the route, we send auth-token in header with token of user.
    For fetching all notes, we simply used middleware fetchUser which will give us user id and
    according to that user id we fetch all notes present in that user's database.
    For this we use find() method to find user id.
    
    For creating a note, we used post method of route,
    also here after using fetchuser middleware, we used the validation so that no one can save empty note in database.
    When saving the note, in note we save a user id send by fetchUser middleware as user: req.user and title, description and tag.
    Then saving that note in database.
    Also for creating a note, we must create a foreigh key in Note module which connected to user id of User module,
    so we created user entry in Note schema and gave it type of mongoose.Schema.Types.ObjectId and also gave ref : "user" to it.
    So it acts like foreign key and connect User schema with Note schema.
    
    For testing, we created new folder in thunderclient's iNotebook with name note
    in note folder,
    we created first get request as fetch all notes and set path to  /api/note/fetchAllNotes
    and also in header setting content-type to application/json
    and auth-token as token
    Also,
    we created a post request as add a note and set path to /api/note/addNote
    and setting header as content-type as application/json and auth-token as token

--- backend/routes/note.js
+++ backend/routes/note.js
@@ -1,8 +1,50 @@
 const express = require("express");
 const router = express.Router();
+const Note = require("../models/Note"); // importing Note schema
+const fetchUser = require("../middleware/fetchUser"); // importing fetchUser middleware to get id from token
+const { body, validationResult } = require("express-validator"); // used validator for adding notes so no empty note added in databasew
 
-router.get("/", (req, res) => {
-  res.json([]);
+// route 1: Get all the notes using : GET "/api/note/fetchAllNotes" , login required
+router.get("/fetchAllNotes", fetchUser, async (req, res) => {
+  try {
+    const notes = await Note.find({ user: req.user }); // find all notes where user is id send by middleware
+    res.json(notes);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("some internal error occured");
+  }
 });
+
+// route 2: Add new note using : POST "/api/note/addNote" , login required
+// here we set input validation after the fetchUser middleware
+router.get(
+  "/addNote",
+  fetchUser,
+  [
+    body("title", "Enter proper title").isLength({ min: 4 }),
+    body("description", "Minimum length is 8 for description").isLength({ min: 8 }),
+  ],
+  async (req, res) => {
+    // check validation is satisfied or not. If not then send error
+    const result = validationResult(req);
+    if (!result.isEmpty()) {
+      result.array().forEach((e) => console.log(e.msg));
+      return res.status(400).send({ errors: result.array() });
+    }
+
+    try {
+      const { title, description, tag } = req.body; // destructing all keys from req.body object
+      const note = new Note({
+        user: req.user,
+        title, description, tag
+      }); // creating a new note with including user id send by fetchUser middleware and saved as user also the title, description and tag from req.body
+      const savedNote = await note.save().then(console.log("Note saved sucessfully")); // saving the note in database and wait till saving note
+      res.send(savedNote);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("some internal error occured");
+    }
+  }
+);
 
 module.exports = router;
