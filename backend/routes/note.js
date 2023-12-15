1f44a077178020e1fc0f5785f275423edee8288a
atul-mandackar <atul007atulmandavkar@gmail.com>
2023-12-15 16:35:35 +0530

    New route for updating a note. For updating a note from database, we use the PUT method. In put method, first argument is path, second is middleware which gave us user id from token, and third argument is (req,res). In path we send the id of note which we want to update. After that, in trycatch box we check the id from path is exist or not using findById method of schema. If not then send file no found error 404. After that, we check that the user id we get from middleware fetchUser and user id of our note (get from note id provided in path) are same or not. (the user id of note is in object form we have convert it into string before checking) If user id is different for both user and note then send unauthorize error 401. After that we destructure the data get from req.body. After that we used findIdAndUpdate method to make update in database.
    For testing,
    we make a new thunderClient PUT request in note folder of iNotebook.
    And gave path like localhost:5000/api/note/updateNote/:id, where :id is id of note which we want to change.
    value for :id is get from database of note, which is a note id not user id.
    Set header, content-type and auth-token to request.
    Then in body, we made a json file of updation in note
    And then save and send it

--- backend/routes/note.js
+++ backend/routes/note.js
@@ -47,4 +47,32 @@ router.post(
   }
 );
 
+
+// route 3: Update the existing note using : PUT "/api/note/updateNote/:id" , login required and :id is id of not which we want to update
+// Here we also check whether the id of user and user id of note is same or not, so that no other user can change another user's note.
+// fetchUser middleware gave us id directly in req.user form , collected from token send in header of request.
+router.put("/updateNote/:id", fetchUser, async (req, res)=> {
+  let note = {};
+  try {
+    note = await Note.findById(req.params.id); // Finding the note from Note database using id mentioned in request as parameter like /updateNote/:id
+  } catch (error) { // If note not availabel then send error, we used try catch because some time wrong params id make problem
+    return res.status(404).send("Not Found");
+  }
+  
+  if(note.user.toString() !== req.user){ // Checking wheter id get from middleware fetchUser and user id of note from Note databse is same or not. If not same then send error.
+    return res.status(401).send("Not Allowed to change");
+  }
+
+  const {title, description, tag} = req.body; // Destructuring all inputs from request body object.
+
+  // to update a note we used findByIdAndUpdate() method available for schema database.In this method first parameter is id of note to be change, second parameter is that object which will set as update in place of original object , third parameter is setting new value current variable (which is optional and if we not set then it will give us old value before updation in variable but change happened in database).
+  // then() method is also used for giving response to console that note is updated.
+  note = await Note.findByIdAndUpdate(req.params.id, {title, description, tag}, {new: true}).then(console.log("note updated"));
+  // Another way is that {$set: newNote} in place of second parameter of findByIdAndUpdate method, but for that before using it we have to create a new variable named newNote as empty object and gave value title, description and tag to that object but this will increase the code.
+
+  res.send(note);
+  
+});
+
+
 module.exports = router;
