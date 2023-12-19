2879d55a6f69c572266927c77896102445b39849
atul-mandackar <atul007atulmandavkar@gmail.com>
2023-12-19 20:54:29 +0530

    useLocation hook of react-router-dom We used useLocation hook along with useEffect, so that we can show the path of Router Switch changed and according to that changed path we can set Link anchor tag to active ( here home and about Links). code for useLocation is : let location = useLocation();   useEffect(() => {     console.log(location.pathname);   }, [location]); and In return, we usedc that location.pathname like: in Link of home ("/") : we set className by className={`nav-link ${location.pathname === "/" ? "active" : ""}`} in Link of about ("/about") : we set className by className={`nav-link ${location.pathname === "/about" ? "active" : ""}`}
    We also modify the code and removed unneccessary code.
    Also in Home component we added a form code from bootstrap to show form for Note entry.
    And also in form from bootstrap , we made corrections according to react (like class=" to className=" and for=" to htmlFor=")

--- src/App.js
+++ src/App.js
@@ -1,27 +1,31 @@
-import './App.css';
+import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import About from "./components/About";
-import NoteState from './context/note/NoteState';
+import NoteState from "./context/note/NoteState";
 
 function App() {
   return (
-    <div className="App">
-      <NoteState> {/* wrap all code inside NoteState component so that each component can use contex api */}
+    <>
+      <NoteState>
+        {" "}
+        {/* wrap all code inside NoteState component so that each component can use contex api */}
         <Router>
           <Navbar />
-          <Switch>
-            <Route exact path="/">
-              <Home />
-            </Route>
-            <Route exact path="/about">
-              <About />
-            </Route>
-          </Switch>
+          <div className="container">
+            <Switch>
+              <Route exact path="/">
+                <Home />
+              </Route>
+              <Route exact path="/about">
+                <About />
+              </Route>
+            </Switch>
+          </div>
         </Router>
       </NoteState>
-    </div>
+    </>
   );
 }
 
