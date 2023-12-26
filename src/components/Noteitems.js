a121574af5df4ba045fe1490e3aed975ac4e341d
atul-mandackar <atul007atulmandavkar@gmail.com>
2023-12-26 15:55:36 +0530

    Use of fontawesome in code for icon use. The first step to use fontawesome kit in index.html file by one line js code in head section : <script src="https://kit.fontawesome.com/5e84f764cb.js" crossorigin="anonymous"></script>
    Then in body we can use the one line code of icons we want to use.
    Like here we used the delete icon and edit icon
    <i className="fa-regular fa-trash-can mx-2"></i> {/* Fontawesome for delete button */}
    <i className="fa-regular fa-pen-to-square mx-2"></i> {/* Fontawesome for edit button */}
    
    We also add style to i tag in index.css to set cursor to pointer and increase the size of icon when over on them.

--- src/components/Noteitems.js
+++ src/components/Noteitems.js
@@ -3,13 +3,17 @@ import React from "react";
 function Noteitems(props) {
   const { note } = props; // destructure note from props send by Note component
   return (
-    <div className="col-md-3 my-3"> {/* code of card is copied from bootstrap and removed unneccessarty part also set outer of card in column class */}
+    <div className="col-md-3 my-3">
+      {" "}
+      {/* code of card is copied from bootstrap and removed unneccessarty part also set outer of card in column class */}
       <div className="card">
         <div className="card-body">
-          <h5 className="card-title">{note.title}</h5>
-          <p className="card-text">
-            {note.description}
-          </p>
+          <div className="d-flex align-items-center"> {/* Flex is used to set all three element in one line and h5 is block element so used flex */}
+            <h5 className="card-title flex-grow-1">{note.title}</h5>
+            <i className="fa-regular fa-trash-can mx-2"></i> {/* Fontawesome for delete button */}
+            <i className="fa-regular fa-pen-to-square mx-2"></i> {/* Fontawesome for edit button */}
+          </div>
+          <p className="card-text">{note.description}</p>
         </div>
       </div>
     </div>
